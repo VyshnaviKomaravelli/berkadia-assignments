6ee33db0bcd56d77833be9e5316fab2ffe9eaaeb
VyshnaviKomaravelli <44829528+VyshnaviKomaravelli@users.noreply.github.com>
2018-12-05 19:18:08 +0530

    Add files via upload

---
+++ week3/api/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { GalleryService } from './gallery.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  private galleryData;
+  private fullMovieDetials;
+  constructor(private galleryService: GalleryService) { }
+  ngOnInit() {
+    this.galleryService.giveImageData().subscribe((res) => {
+      this.galleryData = this.addBaseURL(res['results']);
+      console.log(this.galleryData);
+    });
+
+  }
+  addBaseURL(response) {
+    if (Array.isArray(response)) {
+      response.forEach(element => {
+        if (element.backdrop_path) {
+          element.backdrop_path = 'http://image.tmdb.org/t/p/w300_and_h450_bestv2/' + element.backdrop_path;
+
+        } else {
+          element.backdrop_path = 'http://image.tmdb.org/t/p/w300_and_h450_bestv2' + '/xgbeBCjmFpRYHDF7tQ7U98EREWp.jpg';
+        }
+      });
+    } else {
+        // alert(response.backdrop_path);
+        response.backdrop_path = 'http://image.tmdb.org/t/p/w1280' + response.backdrop_path;
+
+    }
+
+    return response;
+  }
+
+  getFullDetialsOfMovie(movieId) {
+    this.galleryService.getDetails(movieId).subscribe((res) => {
+      // alert(this.addBaseURL(res).backdrop_path);
+      this.fullMovieDetials = this.addBaseURL(res);
+    });
+  }
+
+}
