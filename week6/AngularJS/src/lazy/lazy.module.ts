9186d45d94e525f1d43d55e92c170b1607222f14
VyshnaviKomaravelli <44829528+VyshnaviKomaravelli@users.noreply.github.com>
2019-01-07 19:48:10 +0530

    Add files via upload

---
+++ week6/AngularJS/src/lazy/lazy.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LazyComponent } from './lazy.component';
+import { RouterModule, Routes } from '@angular/router';
+export const routes:Routes=[
+{path:'',component:LazyComponent}
+]
+
+@NgModule({
+  declarations: [LazyComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class LazyModule { }
