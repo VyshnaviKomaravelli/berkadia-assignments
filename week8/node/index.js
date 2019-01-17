e592c97705af18f59d7d6520322679fb36fa8eb0
VyshnaviKomaravelli <44829528+VyshnaviKomaravelli@users.noreply.github.com>
2019-01-17 21:29:55 +0530

    Add files via upload

---
+++ week8/node/index.js
@@ -0,0 +1,22 @@
+'use strict';
+var express = require('express');
+var bodyParser = require('body-parser');
+var driversRoute = require('./routes/drivers');
+var usersRoute = require('./routes/users');
+var bookingRoute = require('./routes/booking');
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  })
+  
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/users', usersRoute);
+app.use('/drivers',driversRoute);
+app.use('/booking', bookingRoute);
+app.listen(PORT, function() {
+  console.log('Contacts server listening on port %s.', PORT);
+});
---
+++ week8/node/knexfile.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = {
+  client: 'mysql',
+  connection: {
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'Drivers'
+  },
+  useNullAsDefault: true,
+};
