3578d8ca347cd90f1987deb19fefe66e9b538709
VyshnaviKomaravelli <44829528+VyshnaviKomaravelli@users.noreply.github.com>
2019-01-19 19:59:37 +0530

    Create questions.sql

---
+++ finalAssessment/database/questions.sql
@@ -0,0 +1,11 @@
+create database questions;
+use questions;
+create table questions1(id int primary key,question varchar(100),o1 varchar(20),o2 varchar(20),o3 varchar(20),o4 varchar(20),co varchar(20));
+insert into questions1 values(1,'Virgin Trains, Virgin Atlantic and Virgin Racing, are all companies owned by which famous entrepreneur? ','Richard Branson
+Alan Sugar','Donald Trump','Bill Gates','Richard Branson');
+insert into questions1 values(2,'Which company did Valve cooperate with in the crea...','Oculus','Google','Razer','HTC','HTC');
+insert into questions1 values(3,'On a dartboard, what number is directly opposite N...',20,12,19,15,19);
+insert into questions1 values(4,'What geometric shape is generally used for stop si...','octagon','hexagon','triangle','square','octagon');
+insert into questions1 values(5,'What is the shape of the toy invented by Hungarian...''cylinder','sphere','cuboid','cube','cube');
+insert into questions1 values(6,'What type of animal was Harambe, who was shot after a child fell into it\u0026#039;s enclosure at the Cincinnati Zoo?','crocodile','gorilla','tiger','monkey','gorilla');
+insert into questions1 values(7,'What is the nickname of the US state of California...','golden state','sunshine state','treasure state','bay state','golden state')
